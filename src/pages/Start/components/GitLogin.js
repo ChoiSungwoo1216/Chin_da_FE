7543e83981b062412eda1e71f6ed5da7ae4fc981
ChoiSungwoo1216 <tjddn8195@naver.com>
2022-07-05 14:40:18 +0900

    일단 커밋

--- src/pages/Start/components/GitLogin.js
+++ src/pages/Start/components/GitLogin.js
@@ -4,13 +4,13 @@ import React from 'react';
 //import {useNavigate} from "react-router-dom";
 
 const GitLogin = (props) => {
-    console.log(props)
+    console.log("hi")
     //const navigate = useNavigate();
     useEffect(()=> {
         let params = new URL(document.location.toString()).searchParams;
         let code = params.get("code"); // 인가코드 받는 부분
         console.log(code);
-        let REDIRECT_URI = "https://localhost:3000/github";
+        let REDIRECT_URI = "http://localhost:3000/github";
         axios.post(`
             `
             , {
