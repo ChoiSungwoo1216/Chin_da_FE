7df0724d6f1f64d8cfe01b1f1bd4937526e573ed
ChoiSungwoo1216 <tjddn8195@naver.com>
2022-07-06 17:42:54 +0900

    메인 페이지 리덕스 부분 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,55 +2,54 @@ import React,{useState} from "react";
 import './Main.css';
 import { useLocation } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
-import {useParams, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 // import { loadChannelAxios } from "../../redux/modules/channel"
 
 export function Main () {
-
-   const [userImg,setUserImg] = useState();
-   const [userName, setUserName] = useState();
-   const [userWin, setUserWin] = useState();
-   const [userLose, setUserLose] = useState();
+   const user = {userName: "player1", userWin:"1", userLose:"2"};
+   const [userInfo, setUserInfo] = useState({});
+   console.log(userInfo)
    const navigate = useNavigate();
-   const dispatch = useDispatch();
+   // const dispatch = useDispatch();
    const channelList = useSelector((state) => state.channel.list);
    // console.log(channelList);
    // 백이랑 이걸 숫자로 보낼지, 문자열로 보낼지 합의 (현재는 문자열)
-   // const { state } = useLocation();
-   // const language = state.language;
-   // const level = state.level;
-   // console.log(language, level);
+   const { state } = useLocation();
+   const language = state.language;
+   const level = state.level;
+   console.log(language, level);
    //    React.useEffect(() => {
    //       dispatch(loadChannelAxios(language, level));
    //   }, [])
-  let {userId} = useParams();
+   const EnterBattle = () =>{
+      navigate(`/battle/${userInfo.channelId}`)
+   }
    return (
       <>
          <div className="mainContainer">
             <main>
                <section>
                   <article className="article">
-                     <h3 className="txt">player1</h3>
                      <div className="profile">
                         <div className="thumbnail circle"></div>
                         <div className="description">
-                           <p className="content">이름 : player1</p>
-                           <p className="content">승/패</p>
+                           <p className="content">이름 : {user.userName}</p>
+                           <p className="content">{user.userWin}승/{user.userLose}패</p>
                         </div>
                      </div>
+                     <button onClick={EnterBattle}>START</button>
                   </article>
                   <aside className="aside">
-                     <h3 className="txt2">player2</h3>
                      <div className="profile2">
                         <div
                            className="thumbnail circle"
-                           onChange={userImg}
+                           style={{backgroundImage:`${userInfo.userImg}`}}
                         ></div>
                         <div className="description">
-                           <p className="content2">이름: {userName}</p>
+                           <p className="content2">이름: {userInfo.userName}</p>
                            <p className="content2">
-                              {userWin}승/{userLose}패
+                              {userInfo.userWin}승/{userInfo.userLose}패
                            </p>
                         </div>
                      </div>
@@ -65,19 +64,7 @@ export function Main () {
                            <div
                               className="scene"
                               key={idx}
-                              onClick={() => {
-                                 let copy = [...list.userImg];
-                                 let copy1 = [...list.userName];
-                                 let copy2 = [...list.userWin];
-                                 let copy3 = [...list.userLose];
-                                 navigate(
-                                    '/Main/' + list.userId,
-                                    setUserImg(copy),
-                                    setUserName(copy1),
-                                    setUserWin(copy2),
-                                    setUserLose(copy3)
-                                 );
-                              }}
+                              onClick={() => {setUserInfo(channelList[idx])}}
                            >
                               <div className="card">
                                  <div className="face front">
