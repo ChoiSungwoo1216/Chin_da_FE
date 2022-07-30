597b36f2d5d1f49fd6d582f74dffa5f457ed705b
ChoiSungwoo1216 <tjddn8195@naver.com>
2022-07-31 01:37:01 +0900

    로그인 시 튜토리얼 보여주기

--- src/pages/Tutorial/TutorialBtn.js
+++ src/pages/Tutorial/TutorialBtn.js
@@ -6,23 +6,27 @@ import hoverSound from "../../audios/BtnHoverSE1.mp3"
 import { useSelector } from "react-redux"
 
 const TutorialBtn = () => {
-   const userSound = useSelector((state) => state.user.sound);
-    const newuser = sessionStorage.getItem("newUser")
+    const userSound = useSelector((state) => state.user.sound);
+    const newuser = sessionStorage.getItem("newUser");
     const [tutoOpen, setTutoOpen] = React.useState(false);
     const open = () => {
         setTutoOpen(true);
     }
-    useEffect(()=>{
-        if (newuser === true){
-            open();
+    useEffect(() => {
+        if (newuser !== null) {
+            const stringNewUser = newuser.toString();
+            console.log(stringNewUser)
+            if (stringNewUser === "true") {
+                open();
+            }
         }
-    },[newuser])
-   const hoverEs = effectSound(hoverSound, userSound.es)
+    }, [newuser])
+    const hoverEs = effectSound(hoverSound, userSound.es)
 
     return (
         <div>
-            {tutoOpen ? (<Tutorial setTutoOpen={setTutoOpen}/>) : (
-            <TutoBtn src="/img/question_btn_black.svg" onClick={open} onMouseOver={()=>hoverEs.play()}/>
+            {tutoOpen ? (<Tutorial setTutoOpen={setTutoOpen} />) : (
+                <TutoBtn src="/img/question_btn_black.svg" onClick={open} onMouseOver={() => hoverEs.play()} />
             )}
         </div>
     )
